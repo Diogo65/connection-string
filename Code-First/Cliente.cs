86fa8e0224879ea040257d6032dea9df5819e122
Diogo Souza de Oliveira <dsouza@vectoritcgroup.com>
2018-08-01 11:24:30 -0300

    first commit

---
+++ Code-First/Cliente.cs
@@ -0,0 +1,8 @@
+﻿namespace Code_First
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Code-First/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //intall EntityFramework
+
+            //Cadastra os clientes
+            var db = new Contexto();
+            db.Cliente.Add(new Cliente() { Nome = "Carlos dos Santos" });
+            db.Cliente.Add(new Cliente() { Nome = "Jose da Silva" });
+            db.Cliente.Add(new Cliente() { Nome = "Antonio das Couves" });
+            db.SaveChanges();
+        }
+    }
+}
